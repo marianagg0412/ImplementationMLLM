3e6f913ee02c76447b7fa85fd8412884d17dae0e
Mariana <marianagg0412@gmail.com>
2024-03-04 09:12:23 -0500

    sentiment service percentage

--- lib/sentiment_service.dart
+++ lib/sentiment_service.dart
@@ -2,6 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SentimentService {
+  List<Map<String, dynamic>> firstUntilPercentage(List<dynamic> nestedList, double targetPercentage) {
+    double currentPercentage = 0.0;
+    List<Map<String, dynamic>> resultList = [];
+
+      for (var element in nestedList) {
+        while(currentPercentage <= targetPercentage) {
+          Map<String, dynamic> elementMap = element as Map<String, dynamic>;
+          double contribution = elementMap['score'];
+          currentPercentage += contribution;
+          resultList.add(elementMap);
+      }
+    }
+    return resultList;
+  }
+
+
   Future<List<Map<String, dynamic>>> querySentiment(String text) async {
     var url = Uri.parse('http://127.0.0.1:5000/api/model-1'); //change url
     var response = await http.post(url,
@@ -10,10 +26,12 @@ class SentimentService {
 
     if (response.statusCode == 200) {
       var list = jsonDecode(response.body) as List;
-      print(list);
+      double targetPercentage = 0.90;
       var nestedList = list[0] as List;
+      print(nestedList);
       List<Map<String, dynamic>> firstThree =
-      nestedList.take(3).map((e) => e as Map<String, dynamic>).toList();
+      firstUntilPercentage(nestedList, targetPercentage);
+      print(firstThree);
       return firstThree;
     } else {
       throw Exception('Failed to query API: ${response.statusCode}');
